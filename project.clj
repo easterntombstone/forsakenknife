cd27bd57f7fb1d89ac24a505fd5fd4fc10505b27
Emory Merryman <emory.merryman@gmail.com>
2015-11-10 15:35:33 -0500

    resolved confusing dependency issue

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "GNU GPL 3"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :plugins [[lein-ancient "0.6.8"] [lein-kibit "0.1.2"] [jonase/eastwood "0.2.1"] [lein-bikeshed "0.2.0"] [lein-cloverage "1.0.6"]]
+  :plugins [[lein-ancient "0.6.8" :exclusions [org.clojure/clojure]] [lein-kibit "0.1.2"] [jonase/eastwood "0.2.1"] [lein-bikeshed "0.2.0"] [lein-cloverage "1.0.6"]]
   :aliases {"omni" ["do" ["clean"] ["test"] ["with-profile" "production" "deps" ":tree"] ["ancient"] ["kibit"] ["bikeshed" "-m" "1000"] ["cloverage"]]}
   )

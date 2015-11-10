d82bb7d1f6a3ac6bcc0117bcaf5d7765540a3535
Emory Merryman <emory.merryman@gmail.com>
2015-11-10 16:26:56 -0500

    use git for version

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject forsakenknife "0.1.0-SNAPSHOT"
+(defproject forsakenknife "ignored because of DRY"
   :description "Generate an ssh key and add entry to ssh config file."
+  :repositories [["local" "file://deploy/loneviper"]]
   :url "git remote add origin git@github.com:easterntombstone/forsakenknife.git"
   :license {:name "GNU GPL 3"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :plugins [[lein-ancient "0.6.8" :exclusions [org.clojure/clojure]] [lein-kibit "0.1.2"] [jonase/eastwood "0.2.1"] [lein-bikeshed "0.2.0"] [lein-cloverage "1.0.6"]]
-  :aliases {"omni" ["do" ["clean"] ["test"] ["with-profile" "production" "deps" ":tree"] ["ancient"] ["kibit"] ["bikeshed" "-m" "1000"] ["cloverage"]]}
+  :plugins [[lein-ancient "0.6.8" :exclusions [org.clojure/clojure]] [lein-kibit "0.1.2"] [jonase/eastwood "0.2.1"] [lein-bikeshed "0.2.0"] [lein-cloverage "1.0.6"] [org.clojars.cvillecsteele/lein-git-version "1.0.0"]]
+  :aliases {"omni" ["do" ["clean"] ["test"] ["with-profile" "production" "deps" ":tree"] ["ancient"] ["kibit"] ["bikeshed" "-m" "1000"] ["cloverage"] ["deploy" "local"]]}
   )

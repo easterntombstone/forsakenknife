2e990021476ed2c387a9fd223d85866a412ad70d
Emory Merryman <emory.merryman@gmail.com>
2015-11-10 22:10:35 -0500

    it appears the dry plugin messes things up

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject forsakenknife "ignored because of DRY"
+(defproject forsakenknife "0.1.0-SNAPSHOT"
   :description "Generate an ssh key and add entry to ssh config file."
-  :repositories [["local" "file://deploy/loneviper" :sign-releases false]]
+  :repositories [["local" "file://loneviper"]]
   :url "https://github.com/easterntombstone/forsakenknife"
   :license {:name "GNU GPL 3"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :plugins [[lein-ancient "0.6.8" :exclusions [org.clojure/clojure]] [lein-kibit "0.1.2"] [jonase/eastwood "0.2.1"] [lein-bikeshed "0.2.0"] [lein-cloverage "1.0.6"] [org.clojars.cvillecsteele/lein-git-version "1.0.0"]]
+  :plugins [[lein-ancient "0.6.8" :exclusions [org.clojure/clojure]] [lein-kibit "0.1.2"] [jonase/eastwood "0.2.1"] [lein-bikeshed "0.2.0"] [lein-cloverage "1.0.6"]]
   :aliases {"omni" ["do" ["clean"] ["test"] ["with-profile" "production" "deps" ":tree"] ["ancient"] ["kibit"] ["bikeshed" "-m" "1000"] ["cloverage"] ["deploy" "local"]]}
   )

bb5aeedda2e91622689d095a27c1c467e69149c1
afnrfcb7 <emory.merryman@gmail.com>
2015-11-26 11:14:42 +0000

    started

--- src/digitalbird/easterntombstone/forsakenknife/core.clj
+++ src/digitalbird/easterntombstone/forsakenknife/core.clj
@@ -1,6 +1,13 @@
 (ns digitalbird.easterntombstone.forsakenknife.core)
+(use '[clojure.java.shell :only [sh]])
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn- internalimpl "The internal implementation method" [host hostname user keycomment configfile keyfile passphrase]
+  (.delete keyfile)
+  (sh "ssh-keygen" "-f" (str keyfile) "-C" keycomment "-P" passphrase)
+  {:passphrase passphrase}
+)
